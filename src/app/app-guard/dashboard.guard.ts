82b701804d97f9a69e236e350fab6fe9f4730445
Avaneesh Mishra <avaneesh.3772@gmail.com>
2022-12-28 17:33:30 -0500

    Angular 13 Project

--- src/app/app-guard/dashboard.guard.ts
+++ src/app/app-guard/dashboard.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanLoad, Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AppCommonService } from '../app-services/app-common.service';
+import { AppInitializerDataService } from '../app-services/app-initializer-data.service';
+import { DialogNotAuthorizedComponent } from '../dialog-not-authorized/dialog-not-authorized.component';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DashboardGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+export class DashboardGuard implements CanLoad {
+  public appConfigData: any;
+
+  constructor( private appInitializerDataService: AppInitializerDataService,
+    private appCommonService: AppCommonService) {
+      this.appConfigData = this.appInitializerDataService.getAppConfigartionData();
   }
-  
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      const checkCondition = this.appConfigData.roles.includes('dashboard')
+      if(checkCondition) {
+           return true;
+      } else {
+           this.appCommonService.openNotAuthorizedDialogBox(DialogNotAuthorizedComponent);
+           return false;
+      }
+    }
 }
+
+
+
--- src/app/app-guard/role.guard.ts
+++ src/app/app-guard/role.guard.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanLoad, Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AppCommonService } from '../app-services/app-common.service';
+import { AppInitializerDataService } from '../app-services/app-initializer-data.service';
+import { DialogNotAuthorizedComponent } from '../dialog-not-authorized/dialog-not-authorized.component';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RoleGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+
+export class RoleGuard implements CanLoad {
+  public appConfigData: any;
+
+  constructor( private appInitializerDataService: AppInitializerDataService,
+    private appCommonService: AppCommonService) {
+      this.appConfigData = this.appInitializerDataService.getAppConfigartionData();
   }
-  
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      const checkCondition = this.appConfigData.roles.includes('rolemanagement')
+      if(checkCondition) {
+           return true;
+      } else {
+        this.appCommonService.openNotAuthorizedDialogBox(DialogNotAuthorizedComponent);
+           return false;
+      }
+    }
 }
+
+
+
